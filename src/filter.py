7c4a6c9317dc62f5137a57b4a4c946d77a97f00a
LitapAI <litapartintel@gmail.com>
2026-03-17 18:13:17 +0530

    Update filter.py

--- src/filter.py
+++ src/filter.py
@@ -1,16 +1,18 @@
+import pandas as pd
+
 def filter_jobs(df, location, role):
-    filtered = df.copy()
+    filtered_df = df.copy()
 
     # --- Filter by Location ---
     if location != "All":
-        filtered = filtered[
-            filtered["India Locations"].str.contains(location, case=False, na=False)
+        filtered_df = filtered_df[
+            filtered_df["India Locations"].str.contains(location, case=False, na=False)
         ]
 
     # --- Filter by Role ---
     if role != "All":
-        filtered = filtered[
-            filtered["Roles Open (typical)"].str.contains(role, case=False, na=False)
+        filtered_df = filtered_df[
+            filtered_df["Roles Open (typical)"].str.contains(role, case=False, na=False)
         ]
 
-    return filtered
+    return filtered_df
